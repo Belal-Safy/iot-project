48f3edf1f631e13f7883c26efceffb4a261e4ace
Belal Safy <belal.aglan2@gmail.com>
2024-05-04 10:58:59 +0300

    Make SuspicionController

---
+++ controllers/SuspicionController.py
@@ -0,0 +1,25 @@
+import os
+import uuid
+from flask import request
+from firebase_admin import db
+from services.ResponseHandler import ResponseHandler
+import time
+
+# ------------------------------------------- Get All Suspicions -------------------------------------------
+def getAllSuspicions():
+    
+# ------------------------------------------- Get Single Suspicion -------------------------------------------
+def getSuspicion(suspicion_id):
+
+
+# ------------------------------------------- Add Suspicion -------------------------------------------
+def addSuspicion():
+
+
+# ------------------------------------------- Update Suspicion -------------------------------------------
+def updateSuspicion(suspicion_id):
+
+
+# ------------------------------------------- Delete Suspicion -------------------------------------------
+def deleteSuspicion(suspicion_id):
+
